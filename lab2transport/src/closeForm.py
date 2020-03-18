86ade8f6841ce23afea5716d9d4a7d7c4fb6283b
kaustika <snufkinson@yandex.ru>
2020-03-18 13:02:33 +0300

    add method that brings problem to closed-form

---
+++ lab2transport/src/closeForm.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def closeForm(matrix:np.ndarray, stock:np.array, need:np.array)-> (np.ndarray, np.array, np.array):
+    """Brings the transportation problem to closed-form by adding fake provider or consumer"""
+    overallNeed = np.sum(need)
+    overallStock = np.sum(stock)
+    rows, cols = matrix.shape
+    delta = overallStock - overallNeed
+    if delta < 0:
+        fakeRow = np.zeros(shape=(1, cols))
+        matrix = np.append(matrix, fakeRow, axis=0)
+        stock = np.append(stock, -delta)
+    if delta > 0:
+        fakeColumn = np.zeros(shape=(rows, 1))
+        matrix = np.append(matrix, fakeColumn, axis=1)
+        need = np.append(need, delta)
+    return matrix, stock, need
