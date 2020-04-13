bd22c0eb87a998142b30973b106b299177845391
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-04-13 18:38:12 +0300

    lab4: add real function visualization

--- lab4gradient/src/main.m
+++ lab4gradient/src/main.m
@@ -6,4 +6,7 @@ system('cd .. && python -m src.GradientDescent src/steps.txt'); % Windows
 f1 = @(X,Y)(X.^2 + Y.^2 - X + 2.* Y);
 plotDescent(f1, 'steps.txt', -1:0.02:0.5, -1:0.02:-0.3)
 
+%---- to visualize our real function, run `GradientDescent.py` ----
+%---- and execute two lines above                              ----
 % f2 = @(X, Y)(X.^2 + Y.^2 + cos(X+3.*Y) - X + 2.*Y); % our real function
+% plotDescent(f2, 'real_function_steps.txt', -0.5:0.02:0.5, -1.3:0.02:0.2)
