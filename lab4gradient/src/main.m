6d75f0ba60e5ad36bc126b0ba734f5d58765a1b0
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-04-16 10:22:02 +0300

    lab4: src: fix mistake with wrong function in visualizer

--- lab4gradient/src/main.m
+++ lab4gradient/src/main.m
@@ -11,10 +11,5 @@ full_cmd = [cmd sprintf(' --x0 %f ', x0) sprintf(' --y0 %f ', y0)...
 system(full_cmd); 
 
 % ---- plotting function and points from `steps.txt` ----
-f1 = @(X,Y)(X.^2 + Y.^2 - X + 2.* Y);
+f1 = @(X, Y)(X.^2 + Y.^2 + cos(X+3.*Y) - X + 2.*Y);
 plotDescent(f1, 'steps.txt')
-
-%---- to visualize our real function, run `GradientDescent.py` ----
-%---- and execute two lines below                              ----
-% f2 = @(X, Y)(X.^2 + Y.^2 + cos(X+3.*Y) - X + 2.*Y); % our real function
-% plotDescent(f2, 'real_function_steps.txt', -0.5:0.02:0.5, -1.3:0.02:0.2)
