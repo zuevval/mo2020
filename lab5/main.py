3c9eff58c8a3622d81231c97f81a67fecea8536d
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-05-11 20:16:00 +0300

    lab5: add real problem (without gradient)

--- lab5/main.py
+++ lab5/main.py
@@ -25,7 +25,36 @@ def example():
     logging.info(res_x)
 
 
+def our_problem():
+    def phi(x: np.array) -> float:
+        phi1 = 3*x[0]**2 + x[1]**2 - 1
+        phi2 = x[0]**2 + (x[1] - 0.5)**2 - 0.5
+        phi3 = 3*x[0]**2 + x[1]**2-x[2]
+        return np.max([phi1, phi2, phi3])
+
+    def phi_subgrad(x: np.array) -> np.array:
+        pass  # TODO
+
+    A = np.array([
+        [1, 0, 0],
+        [-1, 0, 0],
+        [0, 1, 0],
+        [0, -1, 0],
+        [0, 0, 1],
+        [0, 0, -1]
+    ])
+    b = np.array([1/np.sqrt(3), 1/np.sqrt(3), 1,
+                  (1 - np.sqrt(2))/2, -1, 0])
+    c = np.array([0, 0, 1])
+    lp = LinearProblem(A, b, c)
+    res_x = cut_alg(lp, phi, phi_subgrad)
+    logging.info(res_x)
+
+
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    logging.info("------- simple example -------")
     example()
+    logging.info("-------- our problem ---------")
+    our_problem()
 
