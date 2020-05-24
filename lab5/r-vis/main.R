22cc5ed5a898f8786dc229cedfbac2ccfc0e3161
Valerii Zuev <valera.zuev.zva@gmail.com>
2020-05-24 05:52:21 +0300

    lab5:r:update visualizer

--- lab5/r-vis/main.R
+++ lab5/r-vis/main.R
@@ -1,4 +1,5 @@
 library(rgl)
+library("car")
 library("plot3D")
 library(plot3Drgl)
 
@@ -8,9 +9,16 @@ phi <- function(x, y, z) {
   phi3 <- 3*x^2+y^2-z-1
   return(max(c(phi1, phi2, phi3)))
 }
-grid.x <- seq(-sqrt(3), sqrt(3), length = 20)
-grid.y <- seq((1-sqrt(2))/2, 1, length = 10)
-grid.z <- seq(-1, 0, length = 10)
+bounds.ax = -sqrt(3)
+bounds.bx = sqrt(3)
+bounds.ay = (1-sqrt(2))/2-1
+bounds.by = 1
+bounds.az = -1
+bounds.bz = 0
+
+grid.x <- seq(bounds.ax, bounds.bx, length = 40)
+grid.y <- seq(bounds.ay, bounds.by, length = 30)
+grid.z <- seq(bounds.az, bounds.bz, length = 10)
 
 omega.x = c()
 omega.y = c()
@@ -24,20 +32,22 @@ for (x in grid.x){
         omega.x = append(omega.x, x)
         omega.y = append(omega.y, y)
         omega.z = append(omega.z, z)
-        omega.color = append(omega.color, -phi(x, y, z))
+        omega.color = append(omega.color, phi(x, y, z))
       }
     }
   }
 }
 
+# also scatter3Drgl could be used
 scatter3D(x= omega.x,y=omega.y,z=omega.z,  pch = 16, colvar=omega.color)
-plotrgl(xlim = c(-sqrt(3), sqrt(3)),
-        ylim = c((1-sqrt(2))/2, 1),
-        zlim = c(-1, 0),
+plotrgl(xlim = c(bounds.ax, bounds.bx),
+        ylim = c(bounds.ay, bounds.by),
+        zlim = c(bounds.az, bounds.bz),
         xlab = '', ylab = '', zlab = '',
         type = 'n')
 
 # Add planes
-planes3d(0, -1, 0, -1, col = 'red', alpha = 0.6)
-planes3d(0, 1, 0, 0, col = 'orange', alpha = 0.6)
-planes3d(0, -2, 0, 0.25, col = 'blue', alpha = 0.6)
+planes3d(-1.15470054 -0.41421356 -1., 0.7095598854801188, col = 'red', alpha = 0.6)
+planes3d(0.25152852,  0.70118446, -1., 1.107098264201411, col = 'orange', alpha = 0.6)
+planes3d(0.25152852,  0.39570632, -1., 1.0233292240896534, col = 'blue', alpha = 0.6)
+
